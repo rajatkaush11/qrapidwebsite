fb15b0329a9fd9816a10f728252dd10ee619570c
qrapidnow <qrapidnow@gmail.com>
2024-07-24 20:37:03 +0530

    sse integration

--- src/App.jsx
+++ src/App.jsx
@@ -10,220 +10,220 @@ import LoginPage from './components/LoginPage';
 import './App.css';
 
 const App = () => {
-    const [tables, setTables] = useState(Array.from({ length: 15 }, (_, index) => `T${index + 1}`));
-    const [currentPage, setCurrentPage] = useState('Login');
-    const [selectedTable, setSelectedTable] = useState(null);
-    const [tableColors, setTableColors] = useState(Array(15).fill('blank'));
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
-    const [restaurantName, setRestaurantName] = useState('');
-    const [orders, setOrders] = useState({});
+  const [tables, setTables] = useState(Array.from({ length: 15 }, (_, index) => `T${index + 1}`));
+  const [currentPage, setCurrentPage] = useState('Login');
+  const [selectedTable, setSelectedTable] = useState(null);
+  const [tableColors, setTableColors] = useState(Array(15).fill('blank'));
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [restaurantName, setRestaurantName] = useState('');
+  const [orders, setOrders] = useState({});
 
-    useEffect(() => {
-        const token = localStorage.getItem('token');
-        if (token) {
-            console.log('Token found in localStorage, setting authenticated state');
-            setIsAuthenticated(true);
-            setCurrentPage('TableOverview');
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      console.log('Token found in localStorage, setting authenticated state');
+      setIsAuthenticated(true);
+      setCurrentPage('TableOverview');
+    }
+  }, []);
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      fetchRestaurantDetails();
+      const ws = new WebSocket('wss://customerdb.vercel.app/api/websocket');
+
+      ws.onopen = () => {
+        console.log('WebSocket connection opened');
+      };
+
+      ws.onmessage = (event) => {
+        const order = JSON.parse(event.data);
+        console.log('New order received via WebSocket:', order);
+        const tableIndex = tables.indexOf(`T${order.tableNo}`);
+        if (tableIndex !== -1) {
+          updateTableColor(tableIndex, 'blue');
+          setOrders((prevOrders) => ({
+            ...prevOrders,
+            [`T${order.tableNo}`]: order
+          }));
         }
-    }, []);
+      };
 
-    useEffect(() => {
-        if (isAuthenticated) {
-            fetchRestaurantDetails();
-            const ws = new WebSocket('wss://customerdb.vercel.app');
+      ws.onerror = (error) => {
+        console.error('WebSocket error:', error);
+      };
 
-            ws.onopen = () => {
-                console.log('WebSocket connection opened');
-            };
+      ws.onclose = () => {
+        console.log('WebSocket connection closed');
+      };
 
-            ws.onmessage = (event) => {
-                const order = JSON.parse(event.data);
-                console.log('New order received via WebSocket:', order);
-                const tableIndex = tables.indexOf(`T${order.tableNo}`);
-                if (tableIndex !== -1) {
-                    updateTableColor(tableIndex, 'blue');
-                    setOrders((prevOrders) => ({
-                        ...prevOrders,
-                        [`T${order.tableNo}`]: order
-                    }));
-                }
-            };
+      return () => {
+        ws.close();
+      };
+    }
+  }, [isAuthenticated]);
 
-            ws.onerror = (error) => {
-                console.error('WebSocket error:', error);
-            };
+  const fetchRestaurantDetails = async () => {
+    const token = localStorage.getItem('token');
+    console.log('Fetched token from localStorage:', token);
+    if (token) {
+      try {
+        const res = await fetch(`${import.meta.env.VITE_BACKEND_API}/restaurant`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`,
+          },
+        });
 
-            ws.onclose = () => {
-                console.log('WebSocket connection closed');
-            };
-
-            return () => {
-                ws.close();
-            };
+        if (res.ok) {
+          const data = await res.json();
+          console.log('Fetched restaurant details:', data);
+          setRestaurantName(data.name);
+        } else {
+          console.error('Failed to fetch restaurant details');
         }
-    }, [isAuthenticated]);
+      } catch (error) {
+        console.error('Error fetching restaurant details:', error);
+      }
+    }
+  };
 
-    const fetchRestaurantDetails = async () => {
-        const token = localStorage.getItem('token');
-        console.log('Fetched token from localStorage:', token);
-        if (token) {
-            try {
-                const res = await fetch(`${import.meta.env.VITE_BACKEND_API}/restaurant`, {
-                    method: 'GET',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Authorization': `Bearer ${token}`,
-                    },
-                });
+  const addTable = () => {
+    console.log('Adding a new table');
+    setTables([...tables, `T${tables.length + 1}`]);
+    setTableColors([...tableColors, 'blank']);
+  };
 
-                if (res.ok) {
-                    const data = await res.json();
-                    console.log('Fetched restaurant details:', data);
-                    setRestaurantName(data.name);
-                } else {
-                    console.error('Failed to fetch restaurant details');
-                }
-            } catch (error) {
-                console.error('Error fetching restaurant details:', error);
-            }
-        }
-    };
+  const handleLinkClick = (page) => {
+    console.log('Navigating to page:', page);
+    setCurrentPage(page);
+    setSelectedTable(null);
+  };
 
-    const addTable = () => {
-        console.log('Adding a new table');
-        setTables([...tables, `T${tables.length + 1}`]);
-        setTableColors([...tableColors, 'blank']);
-    };
+  const handleSelectTable = (tableNumber) => {
+    console.log('Selected table:', tableNumber);
+    setSelectedTable(tableNumber);
+    setCurrentPage('TableDetails');
+  };
 
-    const handleLinkClick = (page) => {
-        console.log('Navigating to page:', page);
-        setCurrentPage(page);
-        setSelectedTable(null);
-    };
+  const handleBackClick = () => {
+    console.log('Going back to TableOverview');
+    setCurrentPage('TableOverview');
+    setSelectedTable(null);
+  };
 
-    const handleSelectTable = (tableNumber) => {
-        console.log('Selected table:', tableNumber);
-        setSelectedTable(tableNumber);
-        setCurrentPage('TableDetails');
-    };
+  const updateTableColor = (tableIndex, color) => {
+    console.log(`Updating color of table ${tableIndex} to ${color}`);
+    const updatedColors = [...tableColors];
+    updatedColors[tableIndex] = color;
+    setTableColors(updatedColors);
+  };
 
-    const handleBackClick = () => {
-        console.log('Going back to TableOverview');
-        setCurrentPage('TableOverview');
-        setSelectedTable(null);
-    };
+  const handleGenerateKOT = () => {
+    if (selectedTable) {
+      console.log('Generating KOT for table:', selectedTable);
+      const tableIndex = tables.indexOf(selectedTable);
+      updateTableColor(tableIndex, 'running-kot');
+    }
+  };
 
-    const updateTableColor = (tableIndex, color) => {
-        console.log(`Updating color of table ${tableIndex} to ${color}`);
-        const updatedColors = [...tableColors];
-        updatedColors[tableIndex] = color;
-        setTableColors(updatedColors);
-    };
+  const handleGenerateBill = () => {
+    if (selectedTable) {
+      console.log('Generating bill for table:', selectedTable);
+      const tableIndex = tables.indexOf(selectedTable);
+      updateTableColor(tableIndex, 'printed');
+    }
+  };
 
-    const handleGenerateKOT = () => {
-        if (selectedTable) {
-            console.log('Generating KOT for table:', selectedTable);
-            const tableIndex = tables.indexOf(selectedTable);
-            updateTableColor(tableIndex, 'running-kot');
-        }
-    };
+  const handleComplete = () => {
+    if (selectedTable) {
+      console.log('Completing order for table:', selectedTable);
+      const tableIndex = tables.indexOf(selectedTable);
+      updateTableColor(tableIndex, 'paid');
+      setTimeout(() => {
+        updateTableColor(tableIndex, 'blank');
+      }, 6000);
+    }
+  };
 
-    const handleGenerateBill = () => {
-        if (selectedTable) {
-            console.log('Generating bill for table:', selectedTable);
-            const tableIndex = tables.indexOf(selectedTable);
-            updateTableColor(tableIndex, 'printed');
-        }
-    };
+  const handleSubmitRestaurantDetails = () => {
+    console.log('Submitting restaurant details');
+    setCurrentPage('Login');
+  };
 
-    const handleComplete = () => {
-        if (selectedTable) {
-            console.log('Completing order for table:', selectedTable);
-            const tableIndex = tables.indexOf(selectedTable);
-            updateTableColor(tableIndex, 'paid');
-            setTimeout(() => {
-                updateTableColor(tableIndex, 'blank');
-            }, 6000);
-        }
-    };
+  const handleLogin = () => {
+    console.log('Login successful');
+    setIsAuthenticated(true);
+    setCurrentPage('TableOverview');
+  };
 
-    const handleSubmitRestaurantDetails = () => {
-        console.log('Submitting restaurant details');
-        setCurrentPage('Login');
-    };
+  const handleLogout = () => {
+    console.log('Logging out');
+    setIsAuthenticated(false);
+    setCurrentPage('Login');
+    localStorage.removeItem('token');
+  };
 
-    const handleLogin = () => {
-        console.log('Login successful');
-        setIsAuthenticated(true);
-        setCurrentPage('TableOverview');
-    };
-
-    const handleLogout = () => {
-        console.log('Logging out');
-        setIsAuthenticated(false);
-        setCurrentPage('Login');
-        localStorage.removeItem('token');
-    };
-
-    return (
-        <Router>
-            <div>
-                {isAuthenticated && currentPage !== 'Login' && currentPage !== 'Register' && (
-                    <Navbar activePage={currentPage} onLinkClick={handleLinkClick} />
-                )}
-                <div className="content">
-                    <Routes>
-                        <Route path="/" element={
-                            !isAuthenticated ? (
-                                currentPage === 'Login' ? (
-                                    <LoginPage onLogin={handleLogin} onRegister={() => setCurrentPage('Register')} />
-                                ) : (
-                                    <RestaurantDetails onSubmit={handleSubmitRestaurantDetails} />
-                                )
-                            ) : (
-                                currentPage === 'TableOverview' ? (
-                                    <TableOverview
-                                        tables={tables}
-                                        addTable={addTable}
-                                        onSelectTable={handleSelectTable}
-                                        tableColors={tableColors}
-                                        onLogout={handleLogout}
-                                        orders={orders} // Pass orders to TableOverview
-                                    />
-                                ) : currentPage === 'TableDetails' ? (
-                                    <TableDetails
-                                        tableNumber={selectedTable}
-                                        onBackClick={handleBackClick}
-                                        onGenerateKOT={handleGenerateKOT}
-                                        onGenerateBill={handleGenerateBill}
-                                        onComplete={handleComplete}
-                                        orders={orders} // Pass orders to TableDetails
-                                    />
-                                ) : currentPage === 'Dashboard' ? (
-                                    <div>Dashboard Content</div>
-                                ) : currentPage === 'Menu' ? (
-                                    <Menu />
-                                ) : currentPage === 'Orders' ? (
-                                    <div>Orders Content</div>
-                                ) : currentPage === 'Reports' ? (
-                                    <div>Reports Content</div>
-                                ) : (
-                                    <TableOverview
-                                        tables={tables}
-                                        addTable={addTable}
-                                        onSelectTable={handleSelectTable}
-                                        tableColors={tableColors}
-                                        orders={orders} // Pass orders to TableOverview
-                                    />
-                                )
-                            )
-                        } />
-                        <Route path="/category/:categoryId/items" element={<ItemList />} />
-                    </Routes>
-                </div>
-            </div>
-        </Router>
-    );
+  return (
+    <Router>
+      <div>
+        {isAuthenticated && currentPage !== 'Login' && currentPage !== 'Register' && (
+          <Navbar activePage={currentPage} onLinkClick={handleLinkClick} />
+        )}
+        <div className="content">
+          <Routes>
+            <Route path="/" element={
+              !isAuthenticated ? (
+                currentPage === 'Login' ? (
+                  <LoginPage onLogin={handleLogin} onRegister={() => setCurrentPage('Register')} />
+                ) : (
+                  <RestaurantDetails onSubmit={handleSubmitRestaurantDetails} />
+                )
+              ) : (
+                currentPage === 'TableOverview' ? (
+                  <TableOverview
+                    tables={tables}
+                    addTable={addTable}
+                    onSelectTable={handleSelectTable}
+                    tableColors={tableColors}
+                    onLogout={handleLogout}
+                    orders={orders} // Pass orders to TableOverview
+                  />
+                ) : currentPage === 'TableDetails' ? (
+                  <TableDetails
+                    tableNumber={selectedTable}
+                    onBackClick={handleBackClick}
+                    onGenerateKOT={handleGenerateKOT}
+                    onGenerateBill={handleGenerateBill}
+                    onComplete={handleComplete}
+                    orders={orders} // Pass orders to TableDetails
+                  />
+                ) : currentPage === 'Dashboard' ? (
+                  <div>Dashboard Content</div>
+                ) : currentPage === 'Menu' ? (
+                  <Menu />
+                ) : currentPage === 'Orders' ? (
+                  <div>Orders Content</div>
+                ) : currentPage === 'Reports' ? (
+                  <div>Reports Content</div>
+                ) : (
+                  <TableOverview
+                    tables={tables}
+                    addTable={addTable}
+                    onSelectTable={handleSelectTable}
+                    tableColors={tableColors}
+                    orders={orders} // Pass orders to TableOverview
+                  />
+                )
+              )
+            } />
+            <Route path="/category/:categoryId/items" element={<ItemList />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
 };
 
 export default App;
